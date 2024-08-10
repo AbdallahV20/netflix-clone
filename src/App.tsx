4eb557315b6b6e62be9a259c7cfdb8a081684e5d
abdallah ahmed <abdallah2001ahmedp@gmail.com>
2024-08-10 22:39:12 +0300

    code structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center text-primary">
+      <h1 style={{ fontFamily: "NetflixSans" }}>Hello World</h1>
+      <h1>Hello World</h1>
     </div>
   );
-}
+};
 
 export default App;
