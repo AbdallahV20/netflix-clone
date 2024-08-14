0f88c123855af4387d67155238dbfd9798f7c8b0
Abdallah Ahmed <aametwally@ejada.com>
2024-08-14 04:17:51 +0300

    [add] signin

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,8 +1,30 @@
-import './SignIn.css'
+import "./SignIn.css";
+import NetflixLogo from '../../assets/images/netflix-logo.svg'
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <section className="signin-section d-flex justify-content-center vh-100">
+      <div className="container">
+        <img
+          src={NetflixLogo}
+          alt="logo"
+          title="netflix"
+          width={150}
+          className="py-4"
+        />
+        <div className="row">
+          <div className="col-8">
+            <div className="signin-form">
+              <h1>Sign In</h1>
+              <form>
+                <input type="text" className="form-control" />
+                <input type="text" className="form-control" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default SignIn
+export default SignIn;
